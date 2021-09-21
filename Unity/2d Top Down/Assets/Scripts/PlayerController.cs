b52e235c5a869d6bd0c4b1e9cac1bf597ce50223
Cole <coleispro@hotmail.com>
2021-09-21 14:18:06 -0600

    a

--- Unity/2d Top Down/Assets/Scripts/PlayerController.cs
+++ Unity/2d Top Down/Assets/Scripts/PlayerController.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float Speed = 5.0f;
-    public float turnSpeed = 10.0f;
+    public float speed = 50.0f;
+    public float turnSpeed = 100.0f;
     public float hInput;
     public float vInput;
   
+  public float xRange = 7.7f;
+  public float yRange = 4.5f;
     // Update is called once per frame
     void Update()
     {
@@ -18,9 +20,25 @@ public class PlayerController : MonoBehaviour
     //Makes the Player rotate left and right
         transform.Rotate(Vector3.forward * turnSpeed * Time.deltaTime * hInput);
     // Makes the Player move forward and back    
-        transform.Translate(Vector3.forward * Speed * Time.deltaTime * vInput);
+        transform.Translate(Vector3.up * speed * Time.deltaTime * vInput);
 
+    if (transform.position.x > xRange)
+    {
+        transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+    }
+    if (transform.position.x < -xRange)
+    {
+        transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+    }
 
+    if (transform.position.y > yRange)
+    {
+        transform.position = new Vector3(transform.position.x, -yRange, transform.position.z);
+    }
+    if (transform.position.y < -yRange)
+    {
+        transform.position = new Vector3(transform.position.x, yRange, transform.position.z);
+    }
 
 
 
